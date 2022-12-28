48670326212b877caa985314843061b07bf7f021
Geoff Corey <geoff@geoffcorey.com>
2022-12-28 16:27:49 -0500

    Add endovouros_install.sh

---
+++ endevouros_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+pacman -S yay traceroute nmap ttf-hack kitty jq git rust neovim tmux git-delta vcsh dust dog gimp libreoffice-fresh docker gnome-calculator thunar neofetch grim fzf firefox dmenu alacritty prettier stylua flake8 ripgrep python-pynvim xset xclip gpaste fd python-pip flameshot bash-language-server lua-language-server xsane nss-mdns ghostscript gsfonts foomatic-db-engine foomatic-db foomatic-db-ppds foomatic-db-nonfree foomatic-db-nonfree-ppds gutenprint foomatic-db-gutenprint-ppds print-manager
+yay hplip
+yay the_silver_searcher
+yay apache-tools
+yay ttf-hack-nerd 
+yay protonvpn
+yay telegram-desktop
+yay signal-desktop
+yay discord
+yay plex-media-server-plexpass
+npm install -g neovim
+sudo vi /etc/nsswitch.conf
+# hosts: mymachines mdns_minimal [NOTFOUND=return] resolve [!UNAVAIL=return] files myhostname dns
+sudo systemctl enable plexmediaserver.service bluetooth avahi-daemon.service cups-browsed.service cups.service
+sudo systemctl start plexmediaserver.service bluetooth avahi-daemon.service cups-browsed.service cups.service
+#yay brave-bin
+#yay vivaldi
+#yay vivaldi-ffmpeg-codecs
