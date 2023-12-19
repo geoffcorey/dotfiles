39f03628604dad1b34f791961fd57216eb894fd3
Geoff Corey <geoff@geoffcorey.com>
2023-12-19 15:56:46 -0500

    add nvim configs

--- bin/reset-bluetooth.sh
+++ bin/reset-bluetooth.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 bluetoothctl power off
 bluetoothctl power on
-bluetoothctl connect CD:38:23:8D:4C:FD
-bluetoothctl unblock CD:38:23:8D:4C:FD
+#bluetoothctl pairable on
+#bluetoothctl discoverable on
+bluetoothctl connect CD:38:23:8D:4C:FE
+bluetoothctl unblock CD:38:23:8D:4C:FE
+#bluetoothctl scan bredr
+#bluetoothctl pair DC:2C:26:39:D2:DA
+#bluetoothctl trust DC:2C:26:39:D2:DA
+bluetoothctl connect DC:2C:26:39:D2:DA
